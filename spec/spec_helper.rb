0d93444bd2946abfbdddeb81669e07621d5d8349
Daniel E. Doherty <ded-law@ddoherty.net>
2019-12-04 17:18:04 -0600

    Change spec setup to work with files in place.
    Rather than setting up a whole different tree in tmp.  Copy a config template instead.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,48 +13,45 @@ RSpec.configure do |config|
   end
 
   # The following sets up a mock-up of a file system to use for testing under
-  # the project's tmp directory.  It modifies the configuration file there to
-  # include a system ydl directory as well.
-  spec_sys_dir = File.join(__dir__, 'example_files', 'sys')
-  spec_home_dir = File.join(__dir__, 'example_files', 'home')
-  tmp_dir = File.expand_path(File.join(__dir__, '../tmp'))
-  tmp_home = File.join(tmp_dir, 'home')
-  tmp_user_home = File.join(tmp_dir, 'home', 'user')
-  tmp_sys = File.join(tmp_dir, 'sys')
-  tmp_sys_ydl_dir = File.join(tmp_dir, 'sys', 'ydl')
-  tmp_project_dir = File.join(tmp_user_home, 'project', 'subproject')
+  # the project's spec/example_files directory.  It copies the configuration
+  # template file to a configuration file that changes where the system ydl
+  # directory is to be found.
   begin_cwd = Dir.pwd
   begin_home = ENV['HOME']
+  spec_sys_dir = File.join(__dir__, 'example_files', 'sys')
+  spec_home_dir = File.join(__dir__, 'example_files', 'home', 'user')
+  spec_project_dir = File.join(spec_home_dir, 'project', 'subproject')
+  cfg_template = File.join(spec_home_dir, '.ydl', 'config_template.yaml')
+  cfg_file = File.join(spec_home_dir, '.ydl', 'config.yaml')
 
   config.before(:suite) do
-    FileUtils.cp_r(spec_sys_dir, tmp_dir)
+    # FileUtils.cp_r(spec_sys_dir, tmp_dir)
     puts "!" * 80
     puts "Files in #{spec_sys_dir}:"
     puts system("cd #{spec_sys_dir}; pwd; ls -lR")
     puts "!" * 80
-    puts "Copying to ..."
-    puts "+" * 80
-    puts "Files in #{tmp_dir}:"
-    puts system("cd #{tmp_dir}; pwd; ls -lR")
-    puts "+" * 80
-    FileUtils.cp_r(spec_home_dir, tmp_dir)
-    cfg_file = File.join(tmp_user_home, '.ydl', 'config.yaml')
+    # puts "Copying to ..."
+    # puts "+" * 80
+    # puts "Files in #{tmp_dir}:"
+    # puts system("cd #{tmp_dir}; pwd; ls -lR")
+    # puts "+" * 80
+    FileUtils.cp(cfg_template, cfg_file)
     File.open(cfg_file, 'a') do |f|
       cfg_line = <<-EOS
 
-system_ydl_dir: #{tmp_sys_ydl_dir}
+system_ydl_dir: #{spec_sys_dir}
 
       EOS
       f.write(cfg_line)
     end
-    ENV['HOME'] = tmp_user_home
+    ENV['HOME'] = spec_home_dir
     ENV['YDL_CONFIG_FILE'] = cfg_file
-    Dir.chdir(tmp_project_dir)
+    Dir.chdir(spec_project_dir)
   end
 
   config.after(:suite) do
-    FileUtils.rm_rf(tmp_home)
-    FileUtils.rm_rf(tmp_sys)
+    FileUtils.rm_rf(cfg_file)
+    # FileUtils.rm_rf(tmp_sys)
     ENV['HOME'] = begin_home
     Dir.chdir(begin_cwd)
   end
