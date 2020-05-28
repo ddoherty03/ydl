4a574f50b76db50f4b7918dc5635cfdd8ad62744
Daniel E. Doherty <ded-law@ddoherty.net>
2020-05-28 16:47:19 -0500

    Remove debugger line.

--- examples/header.rb
+++ examples/header.rb
@@ -4,7 +4,6 @@ require 'pry'
 LawDoc.document do
   title 'Brief in Support of Plaintiff\'s Motion for Summary Judgment'
   kase Ydl[:cases][:erickson]
-  binding.pry
   on_behalf_of 'plaintiffs', 'nominal'
   signer Ydl[:lawyers][:ded]
   sig_date '2015-11-20'
